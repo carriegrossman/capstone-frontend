acb71df8bf4c1612f7946d411799fc1b0afffc2e
pfarrell18 <66265731+pfarrell18@users.noreply.github.com>
2020-09-16 14:56:35 -0400

    adding search

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Search = ({ currentUser }) => {
+    const [currentShops, setCurrentShops] = useState(undefined);
     const [currentSearch, setCurrentSearch] = useState(undefined);
-    
-    useEffect (() => {
+    const [formData, setFormData] = useState(undefined)
+    useEffect(() => {
         fetch("http://localhost:5000/find", {
             method: "GET",
             headers: {
@@ -13,17 +14,47 @@ const Search = ({ currentUser }) => {
         })
             .then((res) => res.json())
             .then((data) => {
-                setCurrentSearch(data);
+                setCurrentShops(data);
             });
     }, [])
 
+    const  handleChange = (evt) => {
+        setFormData(evt.target.value)
+    }
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+       
+        let filtered = (currentShops.filter((shop) => shop.zipcode === Number(formData)))
+        if (filtered.length ===0) {
+            setCurrentSearch("none")
+        }else{
+            setCurrentSearch(filtered)
+        }
+    }
+
+   
+
+    if (currentSearch) console.log(currentSearch)
     return (
         <div>
-            <h2 className = "title">Shops in my Area</h2>
-            <button className="button is-warning">Search</button>
-            <div className="container">
-                {currentSearch &&
-                    currentSearch.map((shop) => {
+            <h2 className="title">Shops in my Area</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Zipcode</label>
+                <input className="input" type="text" placeholder="Enter Zipcode" onChange={handleChange} />
+                <div className="field is-grouped">
+                    <div className="control">
+                        <button className="button is-warning" type="submit">Search</button>
+                    </div>
+                    <div className="control">
+                        <button className="button is-warning" onClick={()=> setCurrentSearch(undefined)}>Reset</button>
+                    </div>
+                </div>
+            </form>
+                <div className="container">
+                    {currentSearch && currentSearch === "none" && 
+                    <div>Sorry no coffeeshops found with that zipcode!</div>}
+                    {currentSearch && currentSearch !== "none" && currentSearch.map((shop) => {
                         return (
                             <div className="card" key={shop.id}>
                                 <div>{shop.name}</div>
@@ -40,7 +71,26 @@ const Search = ({ currentUser }) => {
                             </div>
                         );
                     })}
-            </div>
+
+                    {!currentSearch && currentShops &&
+                        currentShops.map((shop) => {
+                            return (
+                                <div className="card" key={shop.id}>
+                                    <div>{shop.name}</div>
+                                    <div>{shop.address}</div>
+                                    <div>{shop.city}</div>
+                                    <div>{shop.state}</div>
+                                    <div>{shop.zipcode}</div>
+                                    <Link
+                                        to={`coffeeshop/${shop.id}`}
+                                        className="button is-warning"
+                                    >
+                                        See Homepage
+                </Link>
+                                </div>
+                            );
+                        })}
+                </div>
         </div>
     );
 };
