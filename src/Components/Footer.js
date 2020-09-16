85cc0562928bb0c53b41fd10df4b272115d5c2bd
GarciaFrida <64746413+GarciaFrida@users.noreply.github.com>
2020-09-16 15:29:38 -0400

    mycoffeeshops stuff

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,8 @@ export default function Footer() {
   return (
     <div className="footer-center footer">
       <div className="footer-center">
-        <h2>Hello can you see me </h2>
+        <h2>© Brewsy 2020</h2>
+        <h3>Contact us at: info@Brewsy.com</h3>
       </div>
     </div>
   );
--- src/Components/MyCoffeeShops.js
+++ src/Components/MyCoffeeShops.js
@@ -19,36 +19,39 @@ const MyCoffeeShops = ({ currentUser, setCurrentShop }) => {
   }, [currentUser]);
 
   return (
-    <div>
-      <div className="coffeeshopcontainer">
-        <h2 className="title">My CoffeeShops</h2>
-        <Link className="button" to="/registershop">
-                Register Shop
-              </Link>
-        <hr></hr>
-        <div className="container">
-          {myShops &&
-            myShops.map((shop) => {
-              return (
-                <div className="card" key={shop.id}>
-                  <div>{shop.name}</div>
-                  <div>{shop.address}</div>
-                  <div>{shop.state}</div>
-                  <Link
-                    to={`/${shop.id}/users`}
-                    onClick={() => {
-                      setCurrentShop(shop.id);
-                    }}
-                    className="button"
-                  >
-                    Find User to Stamp
-                  </Link>
-                  <Link to={`coffeeshop/${shop.id}`} className="button">
-                    See Homepage{" "}
-                  </Link>
-                </div>
-              );
-            })}
+    <div className="coffeeshopcontainer">
+      <div className="tile is-ancestor">
+        <div>
+          <h2 className="title tile is-parent">My CoffeeShops</h2>
+          <Link className="button" to="/registershop">
+            Register Shop
+          </Link>
+          <hr></hr>
+          <div className="container tile is-child box">
+            {myShops &&
+              myShops.map((shop) => {
+                return (
+                  <div className="card" key={shop.id}>
+                    <div>{shop.name}</div>
+                    <div>{shop.address}</div>
+                    <div>{shop.state}</div>
+                    <Link
+                      to={`/${shop.id}/users`}
+                      onClick={() => {
+                        setCurrentShop(shop.id);
+                      }}
+                      className="button"
+                    >
+                      Find User to Stamp
+                    </Link>
+                    <Link to={`coffeeshop/${shop.id}`} className="button">
+                      See Homepage{" "}
+                    </Link>
+                  </div>
+                );
+              })}
+          </div>
+          <div className="tile is-child box">Testing</div>
         </div>
       </div>
     </div>
