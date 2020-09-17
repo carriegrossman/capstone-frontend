34a9017ad82a01cbb0ce8529c318c4c834c13a72
GarciaFrida <64746413+GarciaFrida@users.noreply.github.com>
2020-09-17 15:28:24 -0400

    major changes

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,14 +1,29 @@
 import React from "react";
 
-
 export default function About() {
   return (
-   <div>
-   <section className="loginform1">
-   <h1 className="title">About</h1>
-   
-    
- </section>
- </div>
-);
+    <div>
+      <section className="loginform1">
+        <h1 className="title">About</h1>
+        <h2>Welcome to Brewsy!</h2>
+        <p>
+          A day without coffee is like... Just kidding. We have no idea. At
+          Brewsy like big cups and we cannot lie.<br></br>Brewsy is where yelp
+          meets untappd but for all things coffee. This site was made with both
+          coffee lovers and shop owners in mind. Shop owners can register their
+          shops and reward users for visiting their shops. While users can track
+          their visits and receive rewards for every ten visits to their
+          favorite spots. To begin, users must follow the instruction on the
+          sign up page and register. If you’re a shop owner, follow the link at
+          the bottom of the register page to sign up as an owner, and then email
+          us at info@brewsy.com to get verified. Users: From login, you’ll get
+          redirected to “Search/Shops in my Area” where you can find a list of
+          all the local coffee shops that are registered with Brewsy. To further
+          narrow down your search, you can see all the coffee shops around you
+          by searching by zip code. Once, you’ve found a coffee shop, click on
+          the link to be directed to the shop’s homepage.{" "}
+        </p>
+      </section>
+    </div>
+  );
 }
