451df6b84c76b56f4a9054c9edb16771698936b3
Carrie Grossman <carriegrossman@MacBook-Pro.local>
2020-09-17 15:01:50 -0400

    fixed search

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,51 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import locationImg from "../images/location.png";
-
 const Search = ({ currentUser }) => {
-    const [currentShops, setCurrentShops] = useState(undefined);
-    const [currentSearch, setCurrentSearch] = useState(undefined);
-    const [formData, setFormData] = useState("")
-    useEffect(() => {
-        fetch("http://localhost:5000/find", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                setCurrentShops(data);
-            });
-    }, [])
-
-    const handleChange = (evt) => {
-        setFormData(evt.target.value)
-    }
-
-    const Reset = (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        setCurrentSearch(undefined)
-        setFormData("")
-    }
-    console.log({ currentSearch })
-
-
-    const handleSubmit = (evt) => {
-        evt.preventDefault()
-        let filtered = currentShops.filter((shop) => shop.zipcode === Number(formData));
-        setCurrentSearch(filtered)
-    }
-
+  const [currentShops, setCurrentShops] = useState(undefined);
+  const [currentSearch, setCurrentSearch] = useState(undefined);
+  const [formData, setFormData] = useState("");
+  useEffect(() => {
+    fetch("http://localhost:5000/find", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setCurrentShops(data);
+      });
+  }, []);
+  const handleChange = (evt) => {
+    setFormData(evt.target.value);
+  };
   const Reset = (e) => {
     e.preventDefault();
     e.stopPropagation();
     setCurrentSearch(undefined);
-    setFormData(undefined);
+    setFormData("");
   };
   console.log({ currentSearch });
-
   const handleSubmit = (evt) => {
     evt.preventDefault();
     let filtered = currentShops.filter(
@@ -53,62 +34,30 @@ const Search = ({ currentUser }) => {
     );
     setCurrentSearch(filtered);
   };
-
-    if (currentSearch) console.log(currentSearch)
-    return (
-        <div className="loginForm1">
-            <h2 className="title">Shops in my Area</h2>
-            <form onSubmit={handleSubmit}>
-                <label>Zipcode</label>
-                <input className="input" type="text" value={formData} placeholder="Enter Zipcode" onChange={handleChange} />
-                <div className="field is-grouped">
-                    <div className="control">
-                        <button className="button" type="submit">Search</button>
-                    </div>
-                    <div className="control">
-                        <button className="button" onClick={Reset}>Reset</button>
-                    </div>
-                </div>
-            </form>
-            <div className="container">
-                {currentSearch && currentSearch.length === 0 &&
-                    <div>Sorry no coffeeshops found with that zipcode!</div>}
-                {currentSearch && currentSearch.length !== 0 && currentSearch.map((shop) => {
-                    return (
-                        <div className="card" key={shop.id}>
-                            <div className="subtitle">{shop.name}</div>
-                            <div>{shop.address}</div>
-                            <div>{shop.city}</div>
-                            <div>{shop.state}</div>
-                            <div>{shop.zipcode}</div>
-                            <div className='carditem'>
-                            <Link to={`coffeeshop/${shop.id}`} className="trial">
-                                <img className="icon" src={locationImg} alt="shopicon" />
-                                <div>Home</div>
-                            </Link>
-                            </div>
-                        </div>
-                    );
-                })}
-                {!currentSearch && currentShops &&
-                    currentShops.map((shop) => {
-                        return (
-                            <div className="card" key={shop.id}>
-                                <div className="subtitle">{shop.name}</div>
-                                <div>{shop.address}</div>
-                                <div>{shop.city}</div>
-                                <div>{shop.state}</div>
-                                <div>{shop.zipcode}</div>
-                                <div className='carditem'>
-                                <Link to={`coffeeshop/${shop.id}`} className="trial">
-                                    <img className="icon" src={locationImg} alt="shopicon" />
-                                    <div>Home</div>
-                                </Link>
-                                </div>
-                            </div>
-                        );
-                    })}
-            </div>
+  if (currentSearch) console.log(currentSearch);
+  return (
+    <div className="loginForm1">
+      <h2 className="title">Shops in my Area</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Zipcode</label>
+        <input
+          className="input"
+          type="text"
+          value={formData}
+          placeholder="Enter Zipcode"
+          onChange={handleChange}
+        />
+        <div className="field is-grouped">
+          <div className="control">
+            <button className="button" type="submit">
+              Search
+            </button>
+          </div>
+          <div className="control">
+            <button className="button" onClick={Reset}>
+              Reset
+            </button>
+          </div>
         </div>
       </form>
       <div className="container">
@@ -157,5 +106,4 @@ const Search = ({ currentUser }) => {
     </div>
   );
 };
-
 export default Search;
